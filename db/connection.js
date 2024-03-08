5ecff9860bdb293d95a6a2f3edcea04fa2dbe19e
jjoanr <jjoanrodrig@gmail.com>
2024-03-08 19:15:28 +0100

    docker

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'dpp',
-  password: 'dpp123', // Cambia esto por tu contraseña
+  password: 'dpp123',
   database: 'digitalProductPassport'
 });
 
