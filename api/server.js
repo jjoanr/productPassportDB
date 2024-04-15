159cc4aac80c293af57f30297f436b2307a06019
jjoanr <jjoanrodrig@gmail.com>
2024-04-15 18:11:24 +0200

    cors enabled

--- api/server.js
+++ api/server.js
@@ -3,8 +3,21 @@
 // Iniciar el servidor y definir las rutas
 
 const express = require('express');
+const cors = require('cors);
+const rateLimit = require('express-rate-limit');
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+//Rate limiter, max 100 requests per minute
+const limiter = rateLimit ({
+  windowMs: 60*1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+//Habilitar CORS
+app.use(cors());
 
 // Procesar JSONs
 app.use(express.json());
@@ -17,7 +30,7 @@ app.use(express.json());
 const productRoutes = require('./routes/productRoutes');
 app.use('/products', productRoutes);
 
-// Rutas para cuentras de usuarios
+// Rutas para cuentas de usuarios
 const userRoutes = require('./routes/userRoutes');
 app.use('/users', userRoutes);
 
