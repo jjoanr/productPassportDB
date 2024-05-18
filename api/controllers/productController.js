bfd657ccc554a0527c7df42ef0874addf74e75f7
jjoanr <jjoanrodrig@gmail.com>
2024-05-18 10:08:20 +0200

    multer config

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -11,11 +11,11 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    const serialNumber = req.body.serial_number; // Assuming you're sending the serial number in the request body
-    const originalname = file.originalname; // Original name of the file
-    const extension = path.extname(originalname); // Get the extension of the original file
-    const filename = serialNumber + extension; // Concatenate serial number and extension
-    cb(null, filename); // Call the callback with the custom filename
+    const serialNumber = req.body.serial_number;
+    const originalname = file.originalname;
+    const extension = path.extname(file.originalname);
+    const filename = `${serialNumber}${extension}`;
+    cb(null, filename);
   }
 });
 
